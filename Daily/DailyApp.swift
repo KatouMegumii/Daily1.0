3ffcf791a9e6e7afd8d2583e7015f1a1ae44f63d
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2023-04-09 18:16:44 +0100

    initial

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by 丁弈 on 09/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
