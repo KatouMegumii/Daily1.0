6ff015b3ddd55ccc9390d576d74aebb5c459ccbf
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2023-04-11 17:11:07 +0100

    提醒页面视图

---
+++ Daily/TopButtonViews/NoteView.swift
@@ -0,0 +1,17 @@
+//
+//  NoteView.swift
+//  Daily
+//
+//  Created by 丁弈 on 10/04/2023.
+//
+
+import Foundation
+import SwiftUI
+struct NoteView: View{
+var body: some View {
+    Text("This is the second view!")
+        .navigationTitle("Note")
+    }
+}
+
+
