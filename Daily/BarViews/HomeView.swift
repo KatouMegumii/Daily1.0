80f9d93bc49ed86a87a945cf26658572730ae3c8
KatouMegumii <yi.ding-14@postgrad.manchester.ac.uk>
2023-04-11 17:10:59 +0100

    主页视图

---
+++ Daily/BarViews/HomeView.swift
@@ -0,0 +1,15 @@
+//
+//  HomeView.swift
+//  Daily
+//
+//  Created by 丁弈 on 10/04/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        TopBar()
+    }
+}
